78e3b844e1a77fe1ca9a2db343f780ef76fe5822
nethackcom <sashaorlov.2003@mail.ru>
2020-05-06 16:50:18 +0300

    Refresh SOAP.py

--- SOAP.py
+++ SOAP.py
@@ -6,6 +6,7 @@
 
 
 import os
+from zeep import exceptions
 from zeep import Client
 from zeep.transports import Transport
 from requests import Session
@@ -16,20 +17,6 @@ class soap_logic(object):
         self.wsdl = "https://www.ecod.pl/webserv2/EDIservice.asmx?WSDL"
         self.session = Session()
         self.client = Client(self.wsdl, transport=Transport(session=self.session))
-
-        # В переменную big_data заносятся большинство данных вызываемого метода.
-        # Это сделанно для того, чтобы создать некий канал связи между всеми обьектами в классе SOAP.
-        # Переменную обновляет обьект refresh_big_data(), после вызова любого метода веб-сервиса
-        self.big_data = \
-            {
-                "code_error": None,
-                "PartnerIln": None,
-                "DocumentType": None,
-                "DocumentVersion": None,
-                "DocumentStandard": None,
-                "DocumentTest": None,
-                "TrackingId": None
-            }
 
     def refresh_big_data(self, updates):
         self.big_data = \
@@ -44,6 +31,7 @@ class soap_logic(object):
             }
         self.trackback_errors()
 
+    # Метод обработки ошибок
     def trackback_errors(self):
         errors = {
             "00000000": "Операция успешно завершена.",
@@ -55,8 +43,8 @@ class soap_logic(object):
             "00000006": "Ошибка Web.",
             "00000007": "Некорректные параметры."
         }
+
         error = str(self.big_data["code_error"])
-        # print([errors[err] for err in errors if err == error])
         err = lambda errors, error: errors[error]
         print(err(errors, error))
 
@@ -67,70 +55,64 @@ class edi_service_soap_ecod_pl(soap_logic):
     Подробнее вы можете прочить в документации https://www.esphere.ru/assets/download/WebService_Comarch%20EDI.pdf
     '''
 
-    # Подробнее вы можете почитать в документации (https://www.esphere.ru/assets/download/WebService_Comarch%20EDI.pdf)
+    # Подробнее о каждом методе вы можете почитать в документации
+    # (https://www.esphere.ru/assets/download/WebService_Comarch%20EDI.pdf)
 
     # Relationships метод
     # Данный метод возвращает взаимосвязи, определенные для конкретного пользователя в системе
     # ECOD. Взаимосвязи определяют с кем и какого типа документами обменивается пользователь.
     def Relationships(self, *data):
-        request_data = data
-        relationships = self.client.service.Relationships(*request_data)
+        relationships = self.client.service.Relationships(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
         self.refresh_big_data(relationships)
         print(relationships)
 
     # Send метод
     # Данный метод используется для посылки документов.
     def Send(self, *data):
-        request_data = data
-        Send = self.client.service.Send(*request_data)
+        Send = self.client.service.Send(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
         self.refresh_big_data(Send)
         print(Send)
 
     # ListPB метод
     # Метод, позволяющий просмотреть статусы документов, пересылаемых в данный момент.
     def ListPB(self, *data):
-        request_data = data
-        ListPB = self.client.service.ListPB(*request_data)
+        ListPB = self.client.service.ListPB(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
         self.refresh_big_data(ListPB)
         print(ListPB)
 
     # Receive метод
     # Метод, обеспечивающий получение документов.
     def Receive(self, *data):
-        request_data = data
-        Receive = self.client.service.Receive(*request_data)
+        Receive = self.client.service.Receive(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
         self.refresh_big_data(Receive)
         print(Receive)
 
     # ListMB метод
     # Метод возвращает статус документов, которые были доставлены пользователю ECOD.
     def ListMB(self, *data):
-        request_data = data
-        ListMB = self.client.service.ListMB(*request_data)
+        ListMB = self.client.service.ListMB(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
+        print("Error")
         self.refresh_big_data(ListMB)
         print(ListMB)
 
     # ListMBex метод
     # Метод возвращает статус документов, которые были доставлены пользователю ECOD.
     def ListMBex(self, *data):
-        request_data = data
-        ListMBex = self.client.service.ListMBex(*request_data)
+        ListMBex = self.client.service.ListMBex(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
         self.refresh_big_data(ListMBex)
         print(ListMBex)
 
     # ChangeDocumentStatus метод
     # Данный метод дает возможность изменить статус документа (N - new, R - read).
     def ChangeDocumentStatus(self, *data):
-        request_data = data
-        ChangeDocumentStatus = self.client.service.ChangeDocumentStatus(*request_data)
+        ChangeDocumentStatus = self.client.service.ChangeDocumentStatus(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
         self.refresh_big_data(ChangeDocumentStatus)
         print(ChangeDocumentStatus)
 
     # ListPBEx метод
     # Метод возвращает значения статусов отосланных документов.
     def ListPBEx(self, *data):
-        request_data = data
-        ListPBEx = self.client.service.ListPBEx(*request_data)
+        ListPBEx = self.client.service.ListPBEx(os.getenv("NAME_KEY"), os.getenv("PASSWORD_KEY"), *data)
         self.refresh_big_data(ListPBEx)
         print(ListPBEx)
 
@@ -138,6 +120,15 @@ class edi_service_soap_ecod_pl(soap_logic):
 if __name__ == "__main__":
     SOAPClient = edi_service_soap_ecod_pl()
     # code is here...
-    
-    SOAPClient.Relationships("2000000006", "password", 5000)
-    SOAPClient.Send("2000000000006", "password", "2000000000009", "INVOICE", "ECODV0R2", "EDIFACT", "T", "0001", "UNB ... UNZ", 5000)
+    try:
+        SOAPClient.Relationship(5000)
+    except exceptions.ValidationError as excValid:
+        print("Неверные введенные параметры метода: ", str(excValid))
+    except SyntaxError as se:
+        print("Введите правильность вызова метода: ", se)
+    except NameError as e:
+        print("Ошибка параметра: ", str(e))
+    except TypeError as type_method_error:
+        print(type_method_error)
+    except AttributeError as att_method_error:
+        print("Ошибка вызова метода: ", att_method_error)
